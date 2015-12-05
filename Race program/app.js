e56fa9c62d2753718e045ab493c288b1dc60bd85
Dima <sklipus@gmail.com>
2015-12-05 09:22:05 -0800

    commit

--- Race program/app.js
+++ Race program/app.js
@@ -1,94 +1,64 @@
 var app = angular.module('applicationAN',[]);
 
-var ladedData = {
-	teams : [
-		{name: 'SRS',laps:0,time:0},
-		{name: 'Fresh',laps:0,time:0},
-		{name: 'TopNode',laps:0,time:0}
-	],
-	numberLaps : 10
-}
+app.service('teamData', function($rootScope) {
+	var data = [];
+	return {
+		getTeams:function() {
+			return data;
+		},
+		setTeams:function(teams) {
+			data = teams;
+			$rootScope.$broadcast('dataIsUpdated');
+		}
+	}
+});
 
-app.controller('TabController', function(){
-	this.tab = 1;
+app.controller('TabController', function($scope, teamData){
+	var it = this;
+	it.tab = 1;
 
-	this.setTab = function(newValue){
-		this.tab = newValue;
+	it.setTab = function(newValue){
+		it.tab = newValue;
 	};
 
-	this.isSet = function(tabName){
-		return this.tab === tabName;
+	it.isSet = function(tabName){
+		return it.tab === tabName;
 	};
 });
 
-app.controller('TeamsController', function($scope) {
+app.controller('TeamsController', function($scope, teamData) {
 	var it = $scope;
 	it.teams = [];
+	it.$on('dataIsUpdated', function() {
+		it.teams = teamData.getTeams();
+	});
+
 	it.name = '';
+
 	it.addTeam = function(inputStr) {
-		it.teams.push( {name:inputStr,laps:0,time:0});
+		it.teams.push( {name:inputStr,laps:0,time:0,t1:[],max:0});
 		console.log(it.teams);
 		it.name = '';
 	}
+
 	it.delTeam = function(index) {
 		it.teams.splice(index,1);
 	}
 
 	it.save = function() {
-		ladedData = it.teams;
+		teamData.setTeams(it.teams);
 	}
 });
 
-
-app.controller('RaceController', function() {
-
-	this.bestLap = 0;
-	this.indexBest = 0;
-	this.team = ladedData;
-	this.startTime = 0;
-	this.startRace = function () {
-		this.startTime = Date.now();
-	}
-
-
-	this.addLap = function(index){
-		// check if race is started and team laps are less then maxium race
-		if ( this.raceIs() && ( this.team.teams[index].laps < this.team.numberLaps )) {
-
-			//chek best lap
-			var laptime = Date.now() - this.team.teams[index].time - this.startTime;
-			this.bestLap = (
-				laptime <= this.bestLap || this.bestLap === 0 ?
-				laptime :
-				this.bestLap
-			);
-			this.indexBest = index;
-
-			// UPDATE DATE AND LAPS OF ROBOT
-			this.team.teams[index].time = Date.now() - this.startTime;
-			this.team.teams[index].laps = this.team.teams[index].laps + 1;
-			this.team.teams[index].finished = false;
-
-			// CHECK IF ROBOT END THE RACE
-			if (this.team.teams[index].laps === this.team.numberLaps) {
-				this.team.teams[index].finished = true;
-			}
-		}
-	};
-
-	// check if the race is started
-	this.raceIs = function () {
-		return this.startTime > 0 ? true : false;
-	}
-
-});
-
-
-app.controller('QualificationController', function($scope, $interval) {
+app.controller('QualificationController', function($scope, $interval,teamData) {
 
 	var it = $scope;
+	it.teams = [];
+	it.selectedTeam = 0;
+	it.$on('dataIsUpdated', function() {
+		it.teams = teamData.getTeams();
+	});
 
-	it.team = ladedData;
 	it.counter = 0;
 
 	var startTime = 0;
@@ -104,10 +74,23 @@ app.controller('QualificationController', function($scope, $interval) {
 
 	it.stop = function (index) {
 		if(it.counter > 0) {
-			it.team.teams[it.index].t1 = it.counter;
 			$interval.cancel(it.timerId);
+			it.teams[it.index].t1.push(it.counter);
+			sort();
 			it.counter = 0;
 		}
+	}
+
+	function sort() {
+		it.teams[it.index].t1.forEach( function(element, index, array) {
+			if (element > it.teams[it.index].max)
+				it.teams[it.index].max = element;
+		});
+	}
+
+	it.remove = function (index,t) {
+		it.teams[it.index].t1.splice(t,1);
+
 	}
 
 	it.counting = function () {
@@ -115,3 +98,51 @@ app.controller('QualificationController', function($scope, $interval) {
 	}
 });
 
+app.controller('RaceController', function($scope,teamData) {
+	var it = $scope;
+
+	it.startTime = 0;
+	it.numberLaps = 10;
+	it.bestLap = 0;
+	it.indexBest = 0;
+
+	it.teams = [];
+	it.$on('dataIsUpdated', function() {
+		it.teams = teamData.getTeams();
+	});
+
+	it.startRace = function () {
+		it.startTime = Date.now();
+	}
+
+	it.addLap = function(index){
+		// check if race is started and team laps are less then maxium race
+		if ( it.raceIs() && ( it.teams[index].laps < it.numberLaps )) {
+
+			//chek best lap
+			var laptime = Date.now() - it.teams[index].time - it.startTime;
+			it.bestLap = (
+				laptime <= it.bestLap || it.bestLap === 0 ?
+				laptime :
+				it.bestLap
+			);
+			it.indexBest = index;
+
+			// UPDATE DATE AND LAPS OF ROBOT
+			it.teams[index].time = Date.now() - it.startTime;
+			it.teams[index].laps = it.teams[index].laps + 1;
+			it.teams[index].finished = false;
+
+			// CHECK IF ROBOT END THE RACE
+			if (it.teams[index].laps === it.numberLaps) {
+				it.teams[index].finished = true;
+			}
+		}
+	};
+
+	// check if the race is started
+	it.raceIs = function () {
+		return it.startTime > 0 ? true : false;
+	}
+
+});
